a940aae68b26a6650faad7a3f5de81dcf5183b89
EtienneT64 <110019770+EtienneT64@users.noreply.github.com>
2025-01-26 18:51:15 +0200

    End of section 4

--- src/Contracts/AuctionCreated.cs
+++ src/Contracts/AuctionCreated.cs
@@ -1,15 +1,13 @@
-using System;
-using MongoDB.Entities;
+namespace Contracts;
 
-namespace SearchService.Models;
-
-public class Item : Entity
+public class AuctionCreated
 {
+    public Guid Id { get; set; }
     public int ReservePrice { get; set; }
     public required string Seller { get; set; }
     public string? Winner { get; set; }
-    public int? SoldAmount { get; set; }
-    public int? CurrentHighBid { get; set; }
+    public int SoldAmount { get; set; }
+    public int CurrentHighBid { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
     public DateTime AuctionEnd { get; set; }
